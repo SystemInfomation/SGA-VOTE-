848a5a81cc7cccfa1f6c9eee7b8559752444851e
SystemInfomation <forsyth@ahscampus.com>
2026-04-24 18:22:10 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-if (window.location.pathname === '/qr-code') {
+if (window.location.pathname === 'qr-code') {
   window.location.replace('https://docs.google.com/forms/u/0/d/1nzAgcUvFrlL5R6tKMpDrK1rq6xUPaCZ_PY-0NVKCEZo/edit?ntd=1&usp=forms_home&ths=true')
 }
 
